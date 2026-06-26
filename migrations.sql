8cf5fbdd60b3f20396e35ce0cbc337b05625b4ea
Thiago Klein <87211953+ThiagoKleinn@users.noreply.github.com>
2026-06-25 22:44:49 -0300

    Update hash

--- migrations.sql
+++ migrations.sql
@@ -2,3 +2,5 @@
 INSERT INTO usuarios (username, password)
 VALUES ('admin', '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMUPDk4SAlrpVIfBenrAzj96/S')
 ON CONFLICT (username) DO NOTHING;
+
+UPDATE usuarios SET password = '$2b$12$PxurB49mx15S0BJ6WM.U.u7FQbTnzQ1OvdN6QrN3C58S8LgbwluSm' WHERE username = 'admin';
