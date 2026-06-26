1e3f4da7631e86d25617deb15b52432c1956d22a
Thiago Klein <87211953+ThiagoKleinn@users.noreply.github.com>
2026-06-25 22:29:28 -0300

    add table usuarios

--- schema.sql
+++ schema.sql
@@ -41,3 +41,11 @@ funcionariopago INTEGER NOT NULL,
 FOREIGN KEY (idmatricula) REFERENCES matriculas(idmatricula),
 FOREIGN KEY (funcionariopago) REFERENCES funcionarios(idfuncionario)
 );
+
+CREATE TABLE usuarios (
+idusuario SERIAL PRIMARY KEY,
+username VARCHAR(50) UNIQUE NOT NULL,
+password VARCHAR(255) NOT NULL,
+ativo BOOLEAN DEFAULT TRUE,
+criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
